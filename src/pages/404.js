4c8a6d6f353488bd66c303a7645a2a140dc2bca0
Shea McKinney <sherakama@gmail.com>
2021-01-24 23:42:41 -0800

    Changed redirect condition.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { navigate } from "gatsby"
 import { useLocation } from "@reach/router";
-import { parse } from "query-string";
 import Layout from "../components/layout";
 
 // markup
@@ -9,17 +8,12 @@ const NotFoundPage = () => {
 
   // Get out of the url and keep track of specific utm parameters.
   const location = useLocation();
-  const parsedPath = parse(location.pathname);
-  const parsedSearch = parse(location.search);
-  
-  if (parsedSearch.length >= 1 || parsedPath.length >= 1) {
-    navigate("https://alumni.stanford.edu/" + location.pathname + "?" + location.search);
-  }
+  navigate("https://alumni.stanford.edu/" + location.pathname + "?" + location.search);
 
   return (
     <Layout>
     <h1>You have reached the 404 page.</h1>
-    <p>This page should redirect you to alumni.stanford.edu unless you have navigated directly to it.</p>
+    <p>This page should redirect you to alumni.stanford.edu.</p>
     </Layout>
   )
 }
