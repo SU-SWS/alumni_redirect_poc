da0152ae9182e704c37a1058a56483e0ad16f305
Shea McKinney <sherakama@gmail.com>
2021-02-03 13:14:56 -0800

    Try again

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React, { useLayoutEffect} from "react";
 import { useLocation } from "@reach/router";
-import Layout from "../components/layout";
 
 // markup
 const NotFoundPage = () => {
@@ -14,11 +13,7 @@ const NotFoundPage = () => {
 
   return (
     browser && (
-      <Layout>
-        <h1>You have reached the 404 page.</h1>
-        <p>This page should redirect you to pg-dev.stanford.edu.</p>
-        <script>window.location = {}</script>
-      </Layout>
+      <script>window.location = {dest}</script>
     )
   )
 }
